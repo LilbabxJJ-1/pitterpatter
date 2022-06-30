c9f78723a031721194e30c95d5247ee665851cfe
Jade <jaylan.franklin09@gmail.com>
2022-06-30 18:23:01 -0500

    fixed spamming at 0 hours UCT

--- main.py
+++ main.py
@@ -15,12 +15,20 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('-'), case_insensit
 
 @bot.event
 async def on_message(ctx):
+    if ctx.author.bot:
+        return
+    else:
+        pass
     timezone = datetime.timezone.utc
     time = datetime.datetime.now(tz=timezone)
-    guild = bot.get_guild(982394594677358682)
-    chn = guild.get_channel(982394595478478906)
+    guild = bot.get_guild(920057625255747674)
+    chn = guild.get_channel(991806371815243836)
     hour = chn.last_message.created_at + datetime.timedelta(hours=1)
-    send = time.hour >= hour.hour
+    print(chn.last_message.content)
+    print(time, hour.minute)
+    if hour.hour == 0:
+        return
+    send = time.hour >= hour.hour and time.minute >= hour.minute
     if send:
         embed = discord.Embed(title="Come Chat!",
                               description="Aether is quiet,, it must be asleep :o Let's wake it up! Come talk to us!",
